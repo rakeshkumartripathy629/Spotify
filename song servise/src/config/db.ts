452c09dc468d6f79a7c5076769c39fc9c3005101
Rakesh Kumar Tripathy <106573942+rakeshkumartripathy629@users.noreply.github.com>
2025-07-27 07:55:35 +0530

    Update db.ts

--- song servise/src/config/db.ts
+++ song servise/src/config/db.ts
@@ -1,6 +1,6 @@
 import { neon } from "@neondatabase/serverless";
 
 // Replace with your actual Neon connection string
-const DB_URL: string = 'postgresql://neondb_owner:npg_UC4SOlZfGRk3@ep-crimson-sunset-a1t1c5ad-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
+const DB_URL: string = 'add yourURL';
 
 export const sql = neon(DB_URL);
